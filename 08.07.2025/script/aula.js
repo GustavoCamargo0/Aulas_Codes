c8faf03286c65aec006abfb7152cebabd5e38e58
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2025-07-08 16:07:52 -0300

    Add files via upload

---
+++ 08.07.2025/script/aula.js
@@ -0,0 +1,52 @@
+/*Contador Regressivo com HTML
+
+const num = document.getElementById("cont")
+
+for (let cont = 10; cont >= 0; cont--) {
+    num.innerHTML += cont + "<br>";
+
+}
+*/
+
+/* Desafio em sala
+
+let  cont = 1
+for (let index = 0; index <= 64; index++) {
+    alert(cont)
+    cont *= 2
+}
+*/
+
+/* Somar os numeros de 1 à 500
+
+var soma = 0
+for(let cont = 0; cont <= 500; cont++){
+ soma += cont 
+ console.log(soma)
+}
+*/
+
+/* soma impares e pares atè 500
+
+var somapar = 0
+var somaimpar = 0
+for(let cont = 0; cont <= 500; cont++){
+ if(cont % 2 == 0){
+ somapar += cont}
+else{
+    somaimpar += cont
+}
+}
+console.log(somapar)
+console.log(somaimpar)
+*/
+
+/* Soma Multiplos de 3
+
+var somamulti = 0
+for(let cont = 0; cont <= 500; cont++){
+ if(cont % 3 == 0){
+ somamulti += cont}
+}
+console.log(somamulti)
+*/
