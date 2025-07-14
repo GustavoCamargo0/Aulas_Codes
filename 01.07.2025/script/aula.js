10bbf7ee10aa6188487ebd6586b1c2224873ceaa
GustavoCamargo0 <gustavo.fscamargo08@gmail.com>
2025-07-14 14:00:14 -0300

    Update aula.js

--- 01.07.2025/script/aula.js
+++ 01.07.2025/script/aula.js
@@ -1,28 +1,29 @@
+const calc = document.getElementById("calc")
 
-
-function calc(){
+calc.addEventListener("click", function () {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
     const result = document.getElementById("result")
     const opera = document.getElementById("opera")
-    let soma 
-    if(opera.value === "soma" ){
+    let soma
+    if (opera.value === "soma") {
         soma = num1 + num2
     }
-    else if(opera.value === "sub"){
+    else if (opera.value === "sub") {
         soma = num1 - num2
     }
-    else if(opera.value === "multi"){
+    else if (opera.value === "multi") {
         soma = num1 * num2
     }
-    else if(opera.value === "div"){
-        if(num2 === 0){
-            result.innerText = "Disivao por zero não é permitida"; 
+    else if (opera.value === "div") {
+        if (num2 === 0) {
+            result.innerText = "Disivao por zero não é permitida";
         }
-      else{soma = num1 / num2
+        else {
+            soma = num1 / num2
         }
     }
-    
+
 
     result.innerText = soma;
-}
+})
